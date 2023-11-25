d320d29f157d15b8e423ab81cf124c66c48758a6
Alexander Holmes <108624793+AlexanderHolmes0@users.noreply.github.com>
2023-11-25 02:50:36 -0500

    Arcs Go Over Pacific Ocean Now

--- Viz_Sched_App/app.R
+++ Viz_Sched_App/app.R
@@ -19,10 +19,11 @@ locations <- data.frame(cities, latitude, longitude)
 
 to_plot <- left_join(data, locations, by = c("Home" = "cities")) |>
   left_join(locations, by = c("Away" = "cities")) |>
-  rename("lat.home" = latitude.x, "long.home" = longitude.x, lat.away = latitude.y, long.away = longitude.y) |> 
-  mutate(game_num=1:nrow(data),
-         info=paste0("<b>",Home, " - ", Away, "</b>"))
-
+  rename("lat.home" = latitude.x, "long.home" = longitude.x, lat.away = latitude.y, long.away = longitude.y) |>
+  mutate(
+    game_num = 1:nrow(data),
+    info = paste0("<b>", Home, " - ", Away, "</b>")
+  )
 
 ## set your mapbox token here
 
@@ -36,8 +37,9 @@ ui <- page_sidebar(
       }
     "
   ),
-  tags$head(HTML(
-    '<!-- Quick & Dirty HTML Meta Tags -->
+  tags$head(
+    HTML(
+      '<!-- Quick & Dirty HTML Meta Tags -->
 <title>Sports Schedule Viz</title>
 <meta name="description" content="Visualize Your Own Team Schedule!">
 
@@ -63,96 +65,127 @@ ui <- page_sidebar(
 <meta name="twitter:url" content="https://aholmes25.shinyapps.io/Viz_Sched_App/">
 <meta name="twitter:title" content="Sports Schedule Viz">
 <meta name="twitter:description" content="Visualize Your Own Team Schedule!">
-<meta name="twitter:image" content="https://github.com/AlexanderHolmes0/Sports_Sched_Viz/blob/main/Sample.png">'),
-
-tags$link(rel = "shortcut icon", href = "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/World%20Map.png")),
+<meta name="twitter:image" content="https://github.com/AlexanderHolmes0/Sports_Sched_Viz/blob/main/Sample.png">'
+    ),
+    tags$link(rel = "shortcut icon", href = "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/World%20Map.png")
+  ),
   useShinyjs(),
-  title = "Sport Schedule Viz", 
-  theme = bs_theme( bootswatch  = 'lux'),
+  title = "Sport Schedule Viz",
+  theme = bs_theme(bootswatch = "lux"),
   fillable_mobile = TRUE,
   fillable = FALSE,
-  sidebar =sidebar(
-      title = "Map Controls",
-      selectInput(
-        "Team", "Select Team(s)",
-        choices = unique(cities),
-        multiple = TRUE,
-        selected = unique(to_plot$Home)
-      ),
-      selectInput(
-        "Breakdown", "Home/Away/Both",
-        choices = c('Away','Home','All'),
-        selected='All'
-      ),
-      sliderInput('week', 'Match Weeks', min = 1, max = 9,value=c(1,9),step = 1,animate = animationOptions(interval = 3000, loop = TRUE))
-      ,
-      sliderInput('pitch', 'Map Viewing Pitch', min = 0, max = 90, value = 45, step = 1,ticks = FALSE),
-      fileInput('file','Sport Schedule File',width = '100%',accept = c(".txt")),
-      actionButton("reset", "Reset", icon = icon("refresh"))
+  sidebar = sidebar(
+    title = "Map Controls",
+    selectInput(
+      "Team", "Select Team(s)",
+      choices = unique(cities),
+      multiple = TRUE,
+      selected = unique(to_plot$Home)
     ),
-  card(full_screen = TRUE,
-       style = "resize:vertical;",
+    selectInput(
+      "Breakdown", "Home/Away/Both",
+      choices = c("Away", "Home", "All"),
+      selected = "All"
+    ),
+    sliderInput("week", "Match Weeks", min = 1, max = 9, value = c(1, 9), step = 1, animate = animationOptions(interval = 3000, loop = TRUE)),
+    sliderInput("pitch", "Map Viewing Pitch", min = 0, max = 90, value = 45, step = 1, ticks = FALSE),
+    fileInput("file", "Sport Schedule File", width = "100%", accept = c(".txt")),
+    actionButton("reset", "Reset", icon = icon("refresh"))
+  ),
+  card(
+    full_screen = TRUE,
+    style = "resize:vertical;",
     card_header("Route Map"),
-    uiOutput("distPlot") |> withSpinner()),
-  card(full_screen = TRUE,
-       style = "resize:vertical;",
+    uiOutput("distPlot") |> withSpinner()
+  ),
+  card(
+    full_screen = TRUE,
+    style = "resize:vertical;",
     card_header("Schedule"),
-    DTOutput("scheduleDT") |> withSpinner())
+    DTOutput("scheduleDT") |> withSpinner()
+  )
 )
 
 
 
 server <- function(input, output) {
-  set_token("YourToken")
-  observeEvent(input$reset, { 
+  set_token("pk.eyJ1IjoiYWxleGFuZGVyaG9sbWVzMCIsImEiOiJjbHBkNTk1dWkwMTJjMmtxejQ2Z3R6aXFuIn0.9tEJPCxyKZEt13dQjR5LqQ")
+  observeEvent(input$reset, {
     refresh()
   })
-  
+
   uploaded_data <- reactive({
     req(input$file)
-    
+
     ext <- tools::file_ext(input$file$name)
     switch(ext,
-           txt = read.table(input$file$datapath,
-                            sep = "", col.names = c("Home", "Away", "Week"), header = F,
-                            na.strings = "", stringsAsFactors = F) |> 
-             left_join(locations, by = c("Home" = "cities")) |>
-             left_join(locations, by = c("Away" = "cities")) |>
-             rename("lat.home" = latitude.x, "long.home" = longitude.x, lat.away = latitude.y, long.away = longitude.y) |> 
-             mutate(info=paste0("<b>", Home, " - ", Away, "</b>")),
-           validate("Invalid file; Please upload a .txt file")
+      txt = read.table(input$file$datapath,
+        sep = "", col.names = c("Home", "Away", "Week"), header = F,
+        na.strings = "", stringsAsFactors = F
+      ) |>
+        left_join(locations, by = c("Home" = "cities")) |>
+        left_join(locations, by = c("Away" = "cities")) |>
+        rename("lat.home" = latitude.x, "long.home" = longitude.x, lat.away = latitude.y, long.away = longitude.y) |>
+        mutate(info = paste0("<b>", Home, " - ", Away, "</b>")),
+      validate("Invalid file; Please upload a .txt file")
     )
   })
-  
+
   map_data <- reactive({
     Sys.sleep(1)
     req(to_plot)
-    if(input$Breakdown == "All"){
-      m1 <- to_plot |> filter(Home %in% input$Team | Away %in% input$Team)
-    } else if (input$Breakdown == "Home"){
-      m1 <- to_plot |> filter(Home %in% input$Team)
-    } else if (input$Breakdown == "Away"){
-      m1 <- to_plot |> filter(Away %in% input$Team)
+    if (is.null(input$file)) {
+      m1 <- to_plot
+    } else {
+      m1 <- uploaded_data()
     }
-    
-    m1 |> filter(Week >= input$week[1] & Week <= input$week[2])
+
+    if (input$Breakdown == "All") {
+      m1 <- m1 |> filter(Home %in% input$Team | Away %in% input$Team)
+    } else if (input$Breakdown == "Home") {
+      m1 <- m1 |> filter(Home %in% input$Team)
+    } else if (input$Breakdown == "Away") {
+      m1 <- m1 |> filter(Away %in% input$Team)
+    }
+
+    m1 <- m1 |> filter(Week >= input$week[1] & Week <= input$week[2])
+    to_mod1 <- m1
+    to_mod2 <- m1
+    diff <- abs(to_mod1$long.home - to_mod1$long.away)
+    to_mod1$long.home <- ifelse(diff > 180 & to_mod1$long.home > 0, to_mod1$long.home - 360,
+      ifelse(diff > 180 & to_mod1$long.home < 0, to_mod1$long.home + 360, to_mod1$long.home)
+    )
+
+    to_mod2$long.away <- ifelse(diff > 180 & to_mod2$long.away > 0, to_mod2$long.away - 360,
+      ifelse(diff > 180 & to_mod2$long.away < 0, to_mod2$long.away + 360, to_mod2$long.away)
+    )
+    print(rbind(to_mod1, to_mod2))
+    rbind(to_mod1, to_mod2)
   })
-  
+
   output$scheduleDT <- DT::renderDT({
-    if(is.null(input$file)) {to_plot |> select(Home, Away, Week) |> DT::datatable()} 
-    else {uploaded_data() |> select(Home, Away, Week) |> DT::datatable()}
+    if (is.null(input$file)) {
+      to_plot |>
+        select(Home, Away, Week) |>
+        DT::datatable()
+    } else {
+      uploaded_data() |>
+        select(Home, Away, Week) |>
+        DT::datatable()
+    }
   })
-    #to_plot |> select(Home, Away, Week) |> DT::datatable()})
-  
+  # to_plot |> select(Home, Away, Week) |> DT::datatable()})
+
   output$distPlot <- renderUI({
     req(map_data())
     mapdeck(
       style = mapdeck_style("outdoors"),
       pitch = input$pitch,
       height = "80vh",
-      zoom = 50) %>%
+      zoom = 50
+    ) %>%
       add_animated_arc(
-        data = if(is.null(input$file)) {map_data()} else{uploaded_data()},
+        data = map_data(),
         origin = c("long.away", "lat.away"),
         destination = c("long.home", "lat.home"),
         layer_id = "arcs",
@@ -160,7 +193,7 @@ server <- function(input, output) {
         stroke_to = "Home",
         stroke_from_opacity = 1000,
         stroke_to_opacity = 1000,
-        stroke_width = 3,
+        stroke_width = 5,
         tooltip = "info",
         auto_highlight = TRUE,
         legend = TRUE,
@@ -168,9 +201,9 @@ server <- function(input, output) {
           stroke_from = list(title = "Travel Team"), stroke_to = list(title = "Home Team"),
           css = "max-height: 200px;"
         )
-      ) |> 
+      ) |>
       add_arc(
-        data = if(is.null(input$file)) {map_data()} else{uploaded_data()},
+        data = map_data(),
         origin = c("long.away", "lat.away"),
         destination = c("long.home", "lat.home"),
         layer_id = "arcs_static",
